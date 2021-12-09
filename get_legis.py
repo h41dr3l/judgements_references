701f8f985fb88118ad420640f95650cefa9d36b9
h41dr3l <84501740+h41dr3l@users.noreply.github.com>
2021-12-09 17:02:26 +0800

    Uploaded files
    txt files with legislation names and cleaning function to get statute title and statute code

---
+++ get_legis.py
@@ -0,0 +1,85 @@
+import spacy 
+from spacy.matcher import Matcher
+
+#gets the list of titles and code of a statute 
+titles = []
+codes = []
+with open('legis_name.txt', 'r') as f:
+    text = f.read()
+    statutes = text.split('\n')[1:-1]
+    for item in statutes:
+        item = item.split(',')
+        title = item[0].strip()
+        shorthand = item[1].strip()
+        index = 0
+        for i in range(len(shorthand)):
+            if shorthand[i].isdigit():
+                index = i
+                break
+        statute_code = shorthand[:index]
+        titles.append(title)
+        codes.append(statute_code)
+
+
+#test text
+test ="""Burswood Nominees similarly involved the registration of an Australian judgment for 
+gambling debts under the RECJA. In fact, like the present case, the underlying debt giving rise to 
+the Australian judgment in Burswood Nominees was also a debt incurred pursuant to an Australian casino’s 
+CCF (at [3]). The court held that, although the debt arising from the CCF took the form of a loan, it was 
+in substance a claim for money won upon a wager, which would have been caught by s 5(2) of the CLA if the 
+claim had been brought in a Singapore court in the first instance (at [21]–[22]). However, the court went 
+on to hold that, while s 5(2) of the CLA elucidates Singapore’s domestic public policy, s 3(2)(f) of the 
+RECJA requires a higher threshold of public policy to be met in order for the registration of a foreign judgment 
+to be refused (at [24]). The meeting of this higher threshold of public policy, described by the court as 
+“international” public policy, involves asking whether the domestic public policy in question was so important as to 
+form part of the core of essential principles of justice and morality shared by all nations (at [42]). The court held 
+that the domestic public policy encapsulated in s 5(2) of CLA did not meet this higher threshold (at [42]–[46]). 
+It therefore declined to set aside the registration of the Australian judgment."""
+
+test2 = "I will turn to the defence Dr Goh put up at the contempt proceedings before me momentarily. Before I do so, however, it is worth nothing that, prior to the hearing, the Plaintiff commenced bankruptcy examination proceedings under s 83 of the Bankruptcy Act (Cap 20, 2009 Rev Ed) against Dr Goh. This took place on 12 and 13 April 2021 before Assistant Registrar Sim Junhui (the “Examination Proceedings”). During these proceedings, Dr Goh said that the “Chinese businessmen” referenced in his earlier 22 September 2020 email were, in fact, casino junkets. He claimed to be a very big gambler, and that he had lost the Funds in the HSBC-GZ Account on gambling in Macau. Specifically, he stated that both amounts of ¥39,428,736 and ¥46,720,151.01 – forming the subject of the HSBC Orders – were completely lost in this way.  This was first time Dr Goh raised his gambling habit (cross-reference [11] above), and it was a position he maintained during the contempt proceedings."
+
+# section __ of the act
+# while, in
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(test)
+matcher = Matcher(nlp.vocab, validate=True)
+# Add match ID "HelloWorld" with no callback and one pattern
+
+pattern = [
+    [{"LOWER":"of"},{"LOWER":"the"},{"LEMMA":{"IN": codes}}],
+    [{"LOWER":"of"},{"LEMMA":{"IN": codes}}]
+]
+
+matcher.add("FindStatute", pattern)
+
+
+
+# for token in doc:
+#     print(token.text, token.dep_, token.head.text, token.head.pos_,
+#             [child for child in token.children])
+
+
+pattern2 = [nlp(text) for text in titles]
+matcher.add("FindTitle", pattern)
+
+matches = matcher(doc)
+
+matchlist = []
+
+for match_id, start, end in matches:
+    string_id = nlp.vocab.strings[match_id]  # Get string representation
+    span = doc[start-2:end]  # The matched span
+    matchlist.append(span)
+    # print(match_id, string_id, start-2, end, span.text)
+
+for match in matchlist:
+    words = str(match).split(' ')
+    title = ''
+    for word in words:
+        if word in codes:
+            index = codes.index(word)
+            title = titles[index]
+            break
+    match = ' '.join(words) + title
+print(matchlist)
---
+++ scrape_names.py
@@ -0,0 +1,224 @@
+#change webscraper sso file to this 
+import scrapy
+from scrapy.exceptions import CloseSpider
+import datetime
+import requests
+import json 
+from sso_scrape.items import legisItem, subsidItem
+from ..lib import filemgr
+
+
+# Scrapy spider that retrieves either a single Act or all Acts from a contents page and downloads them into a specified folder. 
+# If the user requests all Acts, the response is passed to scrape_all(), which subsequently calls scrape_one() to retrieve the content of the Act.
+# If the user requests a specific Act, the content of the Act is directly retrieved and written to file. 
+
+legis_names = []
+
+with open("./data/legis_name.txt", "w") as f:
+  f.write("Legislation names\n")
+
+class SsoSpider(scrapy.Spider):
+  name = 'sso'
+  allowed_domains = ['sso.agc.gov.sg']
+
+  def __init__(self, retrieve="ALL", sl=False, date=datetime.date.today().strftime("%Y%m%d"), pdf=False, saveTo="./data"):
+    self.retrieve = retrieve
+    self.include_subsid = sl
+    self.date = date
+    self.pdf = pdf
+    self.saveTo = saveTo                # folder name with no slash at the back, e.g. "./data"
+
+  # Scrapy function that defines where the first HTTP request is sent
+  def start_requests(self):
+    # check user-defined save location 
+    if not filemgr.check_save_location(self.saveTo):
+      raise CloseSpider("User abort")
+
+    if self.retrieve == "ALL":
+      # if user does not specify which Act to retrieve, start from the contents page.
+      # TODO: handle date 
+      url = 'https://sso.agc.gov.sg/Browse/Act/Current/All?PageSize=500&SortBy=Title&SortOrder=ASC'
+
+    else:   
+      # if the user has specified a particular Act based on shorthand, start directly at the specified Act.
+      # TODO: handle date 
+      url = f"https://sso.agc.gov.sg/Act/{self.retrieve}"
+      
+      # note on error handling: scrapy automatically skips scraping if the server returns 404, so we don't need to handle that situation for now
+    
+    yield scrapy.http.request.Request(url, self.parse)
+
+  # This parses the initial http response. It is broken up into two main blocks, depending on whether the user requests all Acts or a specific Act.
+  def parse(self, response):
+    # If all Acts are requested, pass the response to scrape_all() to scrape the links to each Act from the contents page
+    # TODO: I think the code from scrape_all can be moved here without a problem but this may be better for readability
+    if self.retrieve == "ALL":
+      yield from self.scrape_all(response)
+
+      # navigate to next page of legislation table of contents
+      relative_url = response.xpath("//a[@aria-label='Next Page']/@href").get()
+      next_page = response.urljoin(relative_url)
+      if next_page:
+        yield scrapy.Request(url=next_page, callback=self.parse)
+    
+    # if user has requested specific Act by shorthand, generate the legisItem directly
+    else:   
+      item = legisItem()
+      link = f"https://sso.agc.gov.sg/Act/{self.retrieve}"
+      item["title"] = response.xpath("//div[@class='legis-title']/div/text()").get()
+      item["shorthand"] = self.retrieve
+      item["link"] = link
+      
+      #finds pdf link
+      pdf_link = response.xpath("//div[@class='legis-title']/a/@href").get()
+      if pdf_link:
+        item["pdf"] = 'Yes'
+      else:
+        item['pdf'] = 'No'
+
+      # grab the desired html and put it in the html attribute of the legisItem (TODO - handle situation where self.pdf == True)
+      item["html"] = self.get_body(response)
+
+      # write to file in folder (defined in CLI argument)
+
+      if self.include_subsid:
+        subsid_link = f"https://sso.agc.gov.sg/Act/{self.retrieve}?DocType=Act&ViewType=Sl&PageIndex=0&PageSize=500"
+        yield scrapy.Request(url=subsid_link, meta= item, callback = self.parse_subsid)
+      
+        self.write_to_file(self.saveTo, item)
+
+      yield item
+
+  def parse_subsid(self, response):
+    item = legisItem()
+    # item['html'] = response.meta.get('html')
+    # item['shorthand'] = response.meta.get('shorthand')
+    # item['link'] = response.meta.get('link')
+    data = response.xpath("//table[@class='table browse-list']/tbody/tr/td/a/text()").extract()
+    res = []
+    for sub in data:
+      res.append(sub)
+    item['subsid'] = res
+    yield item
+    
+    #finds subsidiary legislation and gets the title, link etc of subsid 
+    all_subsid = response.xpath("//table[@class='table browse-list']/tbody/tr")
+    for sub in all_subsid:
+      item = subsidItem()
+      item["short_title"] = sub.xpath("td/a/text()").get()
+      item["order_number"] = sub.xpath("td/text()")[3].get().strip().replace("/", "-")    # no idea why it's 3
+      item["shorthand"] = item["short_title"] + " " + item["order_number"]
+      item["link"] = sub.xpath("td/a/@href").get()
+    
+      request = scrapy.Request(response.urljoin(item["link"]), self.get_subsid)
+      request.meta["subsidItem"] = item
+      yield request
+  
+  def get_subsid(self, response):
+    item = response.meta["subsidItem"]
+    item["html"] = response.xpath("//td[@class='openWd']").get() + self.get_body(response)
+    self.write_to_file(self.saveTo, item)
+    yield item
+    
+
+  # Scrape links to individual Acts from contents page, creating a legisItem for each. 
+  # Then pass each legisItem to scrape_one() to obtain their contents
+  def scrape_all(self, response):   
+    acts =  response.xpath("//table[@class='table browse-list']/tbody/tr")
+    # with open(f"./data/legis_name.txt", "a") as f:
+    #   f.write(f"{len(acts)}\n")
+    # FOR TESTING PURPOSES - to shorten testing process. Remove when finished with testing. 
+    # acts = acts[:10]
+
+    # Create legisItem for each link to an Act in the response, set attributes of legisItem. 
+    for act in acts:
+      item = legisItem()
+      item["title"] = act.xpath(".//td[1]/a[@class='non-ajax']/text()").get()
+      link = f"https://sso.agc.gov.sg{act.xpath('.//a/@href').get()}"     # link to the specific Act
+      item["shorthand"] = link.split("/")[-1]
+      item["link"] = link
+      with open(f"./data/legis_name.txt", "a") as f:
+        f.write(f"{item['title']}, {item['shorthand']}\n")
+      #finds subsid link - 
+      if self.include_subsid:
+        subsid_link = f"https://sso.agc.gov.sg/Act/{item['shorthand']}?DocType=Act&ViewType=Sl&PageIndex=0&PageSize=500"
+        yield scrapy.Request(url=subsid_link, meta= item, callback = self.parse_subsid)
+
+      # pass each legisItem to scrape_one() to write the contents of the Act to a file
+      request = scrapy.Request(url=link, callback=self.scrape_one)
+      request.meta["item"] = item
+      yield request
+
+
+  # Handles response from page of a given Act. Fills in the html attribute of the relevant legisItem and writes the contents to file.
+  # Yields the items so scrapy knows we're done
+  def scrape_one(self, response):   
+    # item = response.meta["item"]
+    # item["html"] = self.get_body(response)
+    # self.write_to_file(self.saveTo, item)
+    # yield item
+    pass
+
+
+  # Grabs the relevant parts from a given response.
+  # For pages that use lazyload, takes the data and makes HTTP calls to get the information, then stitches the retrieved parts together. 
+  # TODO: pages that directly include the content of the Act. 
+  def get_body(self, response):   
+    # TODO: Change the headers here before completion! 
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }  
+
+    # # Gets non-lazy loaded content
+    # data = response.xpath("//div[@class='prov1']").extract()
+    # non_lazy_load =''
+    # for i in data:
+    #   non_lazy_load += i
+    # # Get the parameters with which to request lazyload content
+    # data = json.loads(response.xpath("//div[@class='global-vars']/@data-json")[1].get())
+    # toc_sys_id = data["tocSysId"]
+    # series_ids = [div.attrib["data-term"] for div in response.xpath("//div[@class='dms']")]
+
+    # parts = [non_lazy_load]
+
+    # # Request content in parts
+    # for series_id in series_ids:
+    #   frag_sys_id = data["fragments"][series_id]["Item1"]
+    #   dt_id = data["fragments"][series_id]["Item2"]
+    #   url = "https://sso.agc.gov.sg/Details/GetLazyLoadContent?TocSysId={}&SeriesId={}".format(toc_sys_id, series_id) + \
+    #     "&ValidTime=&TransactionTime=&ViewType=&V=25&Phrase=&Exact=&Any=&Without=&WiAl=&WiPr=&WiLT=&WiSc=" + \
+    #     "&WiDT=&WiDH=&WiES=&WiPH=&RefinePhrase=&RefineWithin=&CustomSearchId=&FragSysId={}&_={}".format(frag_sys_id, dt_id)
+      
+    #   parts.append(download_part(url, headers))
+    
+    # return stitch_parts(parts).replace(u'\xa0', u' ') #ensures no missing spaces
+    pass
+
+
+  # write contents of Act to file
+  def write_to_file(self, saveTo, item):
+    with open(f"{saveTo}/{item['shorthand']}.html", "w") as f:
+      #prints subsid link at the top of .html file
+      if 'subsid' in item:
+        f.write(f"Subsidiary Legislation Link: {item['subsid']}")
+      #handles unicode encode error
+      f.write(item["html"].encode('ascii', errors='ignore').decode('unicode-escape'))
+
+# Downloads additional HTML parts
+# (Not sure whether to put these inside the spider definition or leave them out here)
+def download_part(url, headers):
+    r = requests.get(url, headers=headers)
+    if r.status_code != requests.status_codes.codes.ok:
+        print('URL not found: ' + url)
+        return ''
+    return r.text
+
+# Takes a list of HTML parts and joins them into a single string
+def stitch_parts(parts):
+    first, *remaining = parts
+    insert_idx = first.find('<div class="dms"')
+    return first[:insert_idx] + ''.join(remaining) + first[insert_idx:]
+
+
+
