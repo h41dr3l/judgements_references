aef56814f34c98f9f822a7fe9759a6c9d4921c3e
h41dr3l <84501740+h41dr3l@users.noreply.github.com>
2021-12-13 12:05:01 +0800

    Added test file and made edits

--- get_legis.py
+++ get_legis.py
@@ -43,30 +43,34 @@ with open('legis_name.txt', 'r') as f:
         codes.append(statute_code)
 
 
-with open('2000_SGCA_32.txt', 'r') as f:
-    test = f.read()
+# with open('2000_SGCA_41.txt', 'r') as f:
+#     test = f.read()
 #test text
-# test ="""Burswood Nominees similarly involved the registration of an Australian judgment for 
-# gambling debts under the RECJA. In fact, like the present case, the underlying debt giving rise to 
-# the Australian judgment in Burswood Nominees was also a debt incurred pursuant to an Australian casino’s 
-# CCF (at [3]). The court held that, although the debt arising from the CCF took the form of a loan, it was 
-# in substance a claim for money won upon a wager, which would have been caught by s 5(2) of the CLA if the 
-# claim had been brought in a Singapore court in the first instance (at [21]–[22]). However, the court went 
-# on to hold that, while s 5(2) of the CLA elucidates Singapore’s domestic public policy, s 3(2)(f) of the 
-# RECJA requires a higher threshold of public policy to be met in order for the registration of a foreign judgment 
-# to be refused (at [24]). The meeting of this higher threshold of public policy, described by the court as 
-# “international” public policy, involves asking whether the domestic public policy in question was so important as to 
-# form part of the core of essential principles of justice and morality shared by all nations (at [42]). The court held 
-# that the domestic public policy encapsulated in s 5(2) of CLA did not meet this higher threshold (at [42]–[46]). 
-# It therefore declined to set aside the registration of the Australian judgment."""
+test ="""Burswood Nominees similarly involved the registration of an Australian judgment for 
+gambling debts under the RECJA. In fact, like the present case, the underlying debt giving rise to 
+the Australian judgment in Burswood Nominees was also a debt incurred pursuant to an Australian casino’s 
+CCF (at [3]). The court held that, although the debt arising from the CCF took the form of a loan, it was 
+in substance a claim for money won upon a wager, which would have been caught by s 5(2) of the CLA if the 
+claim had been brought in a Singapore court in the first instance (at [21]–[22]). However, the court went 
+on to hold that, while s 5(2) of the CLA elucidates Singapore’s domestic public policy, s 3(2)(f) of the 
+RECJA requires a higher threshold of public policy to be met in order for the registration of a foreign judgment 
+to be refused (at [24]). The meeting of this higher threshold of public policy, described by the court as 
+“international” public policy, involves asking whether the domestic public policy in question was so important as to 
+form part of the core of essential principles of justice and morality shared by all nations (at [42]). The court held 
+that the domestic public policy encapsulated in s 5(2) of CLA did not meet this higher threshold (at [42]–[46]). 
+It therefore declined to set aside the registration of the Australian judgment."""
 
-# test2 = "I will turn to the defence Dr Goh put up at the contempt proceedings before me momentarily. Before I do so, however, it is worth nothing that, prior to the hearing, the Plaintiff commenced bankruptcy examination proceedings under s 83 of the Bankruptcy Act (Cap 20, 2009 Rev Ed) against Dr Goh. This took place on 12 and 13 April 2021 before Assistant Registrar Sim Junhui (the “Examination Proceedings”). During these proceedings, Dr Goh said that the “Chinese businessmen” referenced in his earlier 22 September 2020 email were, in fact, casino junkets. He claimed to be a very big gambler, and that he had lost the Funds in the HSBC-GZ Account on gambling in Macau. Specifically, he stated that both amounts of ¥39,428,736 and ¥46,720,151.01 – forming the subject of the HSBC Orders – were completely lost in this way.  This was first time Dr Goh raised his gambling habit (cross-reference [11] above), and it was a position he maintained during the contempt proceedings."
+# test = "it has been 121(1) of the CPC states that"
 
 #change to custom tokenizer 
 nlp = en_core_web_sm.load()
 nlp.tokenizer = TokenizerWithFormatting(nlp)
 doc = nlp(test)
 matcher = Matcher(nlp.vocab, validate=True)
+
+
+# for i in doc:
+#     print(i,i.pos_)
 
 #TODO add to patterns 
 pattern = [
@@ -81,8 +85,8 @@ matches = matcher(doc)
 matchlist = []
 for match_id, start, end in matches:
     string_id = nlp.vocab.strings[match_id]  # Get string representation
-    span = doc[start:end]  
-
+    span = doc[start:end]
+    matchlist.append((start, span))  
 
 #get matches for titles
 titles_matcher = PhraseMatcher(nlp.vocab)
