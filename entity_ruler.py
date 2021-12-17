3a238affa64c95fee9aefcb987472d3eeb3c64ed
h41dr3l <84501740+h41dr3l@users.noreply.github.com>
2021-12-17 11:22:37 +0800

    Updated patterns

--- entity_ruler.py
+++ entity_ruler.py
@@ -1,11 +1,21 @@
 import spacy 
 import en_core_web_sm
+from spacy.language import Language
 
 
 def get_text(filename):
     with open(filename, 'r', encoding="utf8") as f:
         test = f.read().replace('\n', ' ')#.split('.')[:-1]
     return test
+
+@Language.component("num_pos_tagger")
+def num_pos_tagger(doc):
+    for token in doc:
+        for ch in token.text:
+            if ch.isdigit():
+                token.pos_ = 'NUM'
+                break
+    return doc   
 
 #gets the list of titles and code of a statute 
 titles = []
@@ -29,44 +39,73 @@ with open('legis_name.txt', 'r') as f:
 
 def extract_ref_sentences(filename):
     test = get_text(filename)
-    #change to custom tokenizer 
     nlp = en_core_web_sm.load()
+    nlp.add_pipe("num_pos_tagger", after="tagger") #TODO: this does not successfuly retag provision numbers into "NUM"
     ruler = nlp.add_pipe("entity_ruler", config={"validate": True}, before='ner')
-
 
     pattern = []
     for title in titles: #create patterns for each title
         title = title.split()
-        prov_pattern = [{"LOWER": "s", "OP":"?"},
+        prov_pattern1 = [{"LOWER": "s", "OP":"?"},
                         {"POS": "NUM", "OP":"+"}, 
                         {"ORTH": ")", "OP": "?"},
-                        {"LOWER":"of"}, 
+                        {"LOWER":"of", "OP":"?"}, 
+                        {"LOWER": "the", "OP": "?"}]
+        prov_pattern2 = [{"LOWER": "s", "OP":"?"},
+                        {"POS": "X", "OP":"+"}, 
+                        {"ORTH": ")", "OP": "?"},
+                        {"LOWER":"of", "OP":"?"}, 
+                        {"LOWER": "the", "OP": "?"}]
+        prov_pattern3 = [{"LOWER": "s", "OP":"?"},
+                        {"POS": "PROPN", "OP":"+"}, 
+                        {"ORTH": ")", "OP": "?"},
+                        {"LOWER":"of", "OP":"?"}, 
                         {"LOWER": "the", "OP": "?"}]
         schedule_pattern = [{"POS": "PROPN", "OP": "?"},
                 {"LOWER":"schedule"}, 
-                {"LOWER":"of"}, 
+                {"LOWER":"of", "OP":"?"},
+                {"LOWER":"to", "OP":"?"}, 
                 {"LOWER": "the", "OP": "?"}]                
         for word in title: #add all the words in the title to the pattern
-            prov_pattern.append({"LOWER": word.lower()})
+            prov_pattern1.append({"LOWER": word.lower()})
+            prov_pattern2.append({"LOWER": word.lower()})
+            prov_pattern3.append({"LOWER": word.lower()})
             schedule_pattern.append({"LOWER": word.lower()})
-        pattern.append({"label": "PROVISION", "pattern": prov_pattern})
+        pattern.append({"label": "PROVISION", "pattern": prov_pattern1})
+        pattern.append({"label": "PROVISION", "pattern": prov_pattern2})
+        pattern.append({"label": "PROVISION", "pattern": prov_pattern3})
         pattern.append({"label": "PROVISION", "pattern": schedule_pattern})
     ruler.add_patterns(pattern)
  
     pattern_codes = []
     for code in codes: #create patterns for each code
-        prov_pattern = [{"LOWER": "s", "OP":"?"},
+        prov_pattern1 = [{"LOWER": "s", "OP":"?"},
                         {"POS": "NUM", "OP":"+"}, 
                         {"ORTH": ")", "OP": "?"},
-                        {"LOWER":"of"}, 
+                        {"LOWER":"of", "OP":"?"}, 
+                        {"LOWER": "the", "OP": "?"},
+                        {"TEXT": code}]
+        prov_pattern2 = [{"LOWER": "s", "OP":"?"},
+                {"POS": "X", "OP":"+"}, 
+                {"ORTH": ")", "OP": "?"},
+                {"LOWER":"of", "OP":"?"}, 
+                {"LOWER": "the", "OP": "?"},
+                {"TEXT": code}]
+        prov_pattern3 = [{"LOWER": "s", "OP":"?"},
+                        {"POS": "PROPN", "OP":"+"}, 
+                        {"ORTH": ")", "OP": "?"},
+                        {"LOWER":"of", "OP":"?"}, 
                         {"LOWER": "the", "OP": "?"},
                         {"TEXT": code}]
         schedule_pattern = [{"POS": "PROPN", "OP": "?"},
                 {"LOWER":"schedule"}, 
-                {"LOWER":"of"}, 
+                {"LOWER":"of", "OP":"?"},
+                {"LOWER":"to", "OP":"?"}, 
                 {"LOWER": "the", "OP": "?"},
                 {"TEXT": code}]                
-        pattern_codes.append({"label": "PROVISION", "pattern": prov_pattern})
+        pattern_codes.append({"label": "PROVISION", "pattern": prov_pattern1})
+        pattern_codes.append({"label": "PROVISION", "pattern": prov_pattern2})
+        pattern_codes.append({"label": "PROVISION", "pattern": prov_pattern3})
         pattern_codes.append({"label": "PROVISION", "pattern": schedule_pattern})
     ruler.add_patterns(pattern_codes)
 
@@ -74,6 +113,8 @@ def extract_ref_sentences(filename):
     for ent in doc.ents:
         if ent.label_ == "PROVISION":
             print(ent.text, ent.label_)
+    # for token in doc:
+    #     print(token, token.pos_)
     spacy.displacy.serve(doc, style="ent")
 
 
